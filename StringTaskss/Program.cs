28b66a3d5a06cd0c73ef44bf92f292116905227b
huseynManaf <huseynim@code.edu.az>
2023-01-15 07:18:27 -0800

    init

---
+++ StringTaskss/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace StringTaskss
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(StringTask.Ceaser(1, "abc"));
+        }
+    }
+}
